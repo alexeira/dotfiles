0e281061e07957627369d8610ace224b6aceb8d0
alexeira <alex.pincheira06@gmail.com>
2023-06-24 23:23:04 -0400

    created alias for git commands

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,3 +1,6 @@
+# Remove Defaults
+rename-item alias:\gc gk -force
+
 # PSReadLine
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
@@ -14,6 +17,15 @@ Import-Module -Name Terminal-Icons
 # Env
 $env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
 
+# Functions Git Alias
+function getStatus { git status }
+function getAdd { git add $args }
+function getCommit { git commit -m $args }
+
+# Misc
+function backDir { cd .. }
+function dirProjects { cd projects/ ; ll }
+
 # Alias
 Set-Alias vim nvim
 Set-Alias ll ls
@@ -21,3 +33,12 @@ Set-Alias g git
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+# Alias Name Value
+Set-Alias -Name .. -Value backDir
+Set-Alias -Name cdp -Value dirProjects
+
+Set-Alias -Name gs -Value getStatus
+Set-Alias -Name ga -Value getAdd
+Set-Alias -Name gc -Value getCommit
+
