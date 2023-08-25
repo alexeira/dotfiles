da937f9a10c35cb3bdf61d0fc31123436568b93e
alexeira <alex.pincheira06@gmail.com>
2023-08-24 23:25:40 -0400

    feat(cli): create function for eslint on react-ts

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -8,11 +8,9 @@ Set-PSReadLineOption -BellStyle None
 Set-PSReadLineOption -PredictionSource History
 
 # Icons
-
 Import-Module posh-git
 $omp_config = Join-Path $PSScriptRoot ".\alexeira.omp.json"
 oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
-
 Import-Module -Name Terminal-Icons
 
 # Env
@@ -27,11 +25,15 @@ function getPull { git pull $args }
 function getBranch { git branch $args }
 function getSwitch { git switch $args }
 
+# Functions CLI
+function eslintReactTs { pnpm add -D prettier eslint eslint-config-prettier eslint-config-standard eslint-plugin-import eslint-plugin-node eslint-plugin-n eslint-plugin-prettier eslint-plugin-promise eslint-plugin-react eslint-plugin-react-hooks @typescript-eslint/parser @typescript-eslint/eslint-plugin }
+
 # Misc
 function backDir { cd .. }
 function dirProjects { cd projects/ ; ll }
 function toNeovimFolder { cd appdata/local/nvim ; ll }
 function toPowerShellFolder { cd .config/powershell/ ; ll }
+function toHelixFolder { cd appdata/roaming/helix ; ll }
 
 # Alias
 Set-Alias vim nvim
@@ -45,6 +47,7 @@ Set-Alias -Name .. -Value backDir
 Set-Alias -Name cdp -Value dirProjects
 Set-Alias -Name cdvim -Value toNeovimFolder
 Set-Alias -Name cdpwsh -Value toPowerShellFolder
+Set-Alias -Name cdhx -Value toHelixFolder
 
 # Alias Git
 Set-Alias g git
@@ -55,3 +58,6 @@ Set-Alias -Name gc -Value getCommit
 Set-Alias -Name gp -Value getPull
 Set-Alias -Name gb -Value getBranch
 Set-Alias -Name gsw -Value getSwitch
+
+# Eslint & Prettier
+Set-Alias -Name eslint-react-ts -Value eslintReactTs
